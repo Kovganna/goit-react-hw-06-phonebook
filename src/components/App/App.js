997ccdc8f7e3172839b2bfe4ce405f601323de1f
Ganna <annakovalenko168@gmail.com>
2021-11-08 23:59:18 +0200

    added LocalStorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,32 @@ import './App.css';
 import ContactForm from '../ContactForm/ContactForm';
 import Filter from '../Filter/Filter';
 import ContactList from '../ContactList/ContactList';
+import dataContacts from '../../FileJson/FileJson.json';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: dataContacts,
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('unmount');
+  }
 
   formSubmitHandler = data => {
     const allReadyPresentContact = this.state.contacts.some(
@@ -49,6 +64,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('APP componentDidMount');
     const { filter } = this.state;
     const { formSubmitHandler, handleFilterChange, delContact } = this;
     const getContact = this.handleFilterContact();
